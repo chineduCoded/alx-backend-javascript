76db8fb2eabd02d126638d0bebec8c2ddc457b77
chineduCoded <chinedujohn17@gmail.com>
2023-06-27 22:43:31 +0000

    modified file

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,14 +1,10 @@
 export default function handleResponseFromAPI(promise) {
-  promise
-    .then(response => {
+  const body = { status: 200, body: 'success' };
+
+  return promise
+    .then(() => body)
+    .catch(() => new Error())
+    .finally(() => {
       console.log('Got a response from the API');
-      return {
-        status: 200,
-        body: 'success'
-      };
-    })
-    .catch(error => {
-      console.log('Got a response from the API');
-      return new Error();
     });
 }
