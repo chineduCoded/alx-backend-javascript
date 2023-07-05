a80e7ab7c593ce391353ae83a82dd8a828231a5e
chineduCoded <chinedujohn17@gmail.com>
2023-07-05 03:10:57 +0000

    Updated task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,17 @@
 export default function cleanSet(set, startString) {
-  if (!startString || typeof startString !== 'string' || typeof set !== 'object') return '';
-  return [...set]
-    .filter((el) => el.startsWith(startString))
-    .map((el) => el.slice(startString.length))
-    .join('-');
+  if (typeof startString !== 'string' || typeof set !== 'object' || !set.size) {
+    return '';
+  }
+
+  let cleanedValues = '';
+  for (const value of set) {
+    if (value.startsWith(startString)) {
+      if (cleanedValues.length > 0) {
+        cleanedValues += '-';
+      }
+      cleanedValues += value.slice(startString.length);
+    }
+  }
+
+  return cleanedValues;
 }
