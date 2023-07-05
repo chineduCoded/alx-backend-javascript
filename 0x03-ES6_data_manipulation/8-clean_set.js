6206b44b3358a1d672fae1215c29311877009f7a
chineduCoded <chinedujohn17@gmail.com>
2023-07-05 02:52:15 +0000

    Clean set

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,7 @@
+const cleanSet = (set, startString) => {
+  const filteredValues = [...set].filter((value) => value.startsWith(startString));
+  const cleanedString = filteredValues.map((value) => value.slice(startString.length)).join('-');
+  return cleanedString;
+};
+
+export default cleanSet;
